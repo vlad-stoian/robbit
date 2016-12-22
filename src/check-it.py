d10c514a71c25344313d78e5abb8d49ee6418a2f
Vlad Stoian <vstoian@pivotal.io>
2016-12-22 17:44:39 +0000

    Add script that takes postfacto action items and prints them
    Signed-off-by: Carlo Colombo <ccolombo@pivotal.io>

--- src/check-it.py
+++ src/check-it.py
@@ -1,3 +1,33 @@
-
+import urllib.request
+import sys
+import json
 
 print("Hello There!")
+
+script_path, slack_token, postfacto_token, retro_id = sys.argv
+
+
+print("Debug!")
+print(slack_token)
+print(postfacto_token)
+print(retro_id)
+
+headers = {
+    'authorization': postfacto_token,
+    'content-type': 'application/json',
+    'Accept': "*/*",
+}
+url = 'https://retro-api.cfapps.io/retros/' + retro_id
+request = urllib.request.Request(url, headers=headers)
+
+response = urllib.request.urlopen(request)
+
+retro = json.loads(response.read().decode('utf-8'))
+
+action_items = retro["retro"]["action_items"]
+
+sorted_action_items = sorted([item for item in action_items if not item["done"]], key=lambda x: x["created_at"])
+
+message = "\n".join([item["description"] for item in sorted_action_items])
+
+print(message)
