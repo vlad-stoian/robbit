b6ebec1f36847b1d8a493aa24a07fb38c98a1523
Carlo Colombo <ccolombo@pivotal.io>
2016-12-23 08:46:15 +0000

    Add a nice morning retro message with configurable channel
    Signed-off-by: Vlad Stoian <vstoian@pivotal.io>

--- src/check-it.py
+++ src/check-it.py
@@ -1,16 +1,17 @@
 import urllib.request
+import urllib.parse
 import sys
 import json
 
 print("Hello There!")
 
-script_path, slack_token, postfacto_token, retro_id = sys.argv
+script_path, retro_id, postfacto_token, slack_token, slack_channel = sys.argv
 
-
-print("Debug!")
+print("<debug>")
 print(slack_token)
 print(postfacto_token)
 print(retro_id)
+print("</debug>")
 
 headers = {
     'authorization': postfacto_token,
@@ -19,15 +20,29 @@ headers = {
 }
 url = 'https://retro-api.cfapps.io/retros/' + retro_id
 request = urllib.request.Request(url, headers=headers)
-
 response = urllib.request.urlopen(request)
 
 retro = json.loads(response.read().decode('utf-8'))
-
 action_items = retro["retro"]["action_items"]
-
 sorted_action_items = sorted([item for item in action_items if not item["done"]], key=lambda x: x["created_at"])
 
-message = "\n".join([item["description"] for item in sorted_action_items])
+good_morning = ["*Good morning, fellow rabbits*:rabbit2:",]
+retro_items = ["Retro items:", "```"] + [item["description"] for item in sorted_action_items] + ["```"]
 
+message = "\n".join(good_morning + retro_items) 
 print(message)
+
+
+#TODO: Maybe add personal @user to annoy everyone
+slack_url = "https://slack.com/api/chat.postMessage"
+
+slack_data = {
+    "token": slack_token,
+    "channel": slack_channel,
+    "text": message,
+    "username": "robbit",
+    "icon_emoji": ":rabbit:",
+}
+
+urllib.request.urlopen(slack_url, data=urllib.parse.urlencode(slack_data).encode('utf-8'))
+
