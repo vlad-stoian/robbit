c6c2229d09bde9bce0f90a898aa9b369d9818c30
Vlad Stoian <vstoian@pivotal.io>
2017-07-20 14:03:46 +0100

    Shuffle concourse configs around

---
+++ concourse/set-pipeline.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if lpass status; then
+  fly -t rabbit sp -p slacking-robbit -c robbit.yml \
+    --load-vars-from <(lpass show 'Shared-London Services/rabbitmq/robbit-secrets' --notes) \
+    --var github-private-key="$(lpass show 'Shared-London Services/rabbitmq/robbit-github-key' --field="Private Key")"
+fi
--- set-pipeline.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if lpass status; then
-  fly -t zumba sp -p rabbitmq-slacking-robbit -c robbit.yml --load-vars-from <(lpass show 'Shared-London Services/rabbitmq/robbit-secrets' --notes)
-fi
