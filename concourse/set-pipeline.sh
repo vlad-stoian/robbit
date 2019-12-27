b03c9c91e0b3844d111f629b278fc544870cbb97
Fei Yang <fyang@pivotal.io>
2019-12-27 12:33:12 -0800

    Adapt Robbit pipeline to `hushhouse`

--- concourse/set-pipeline.sh
+++ concourse/set-pipeline.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if lpass status; then
-  fly -t rabbit sp -p slacking-robbit -c concourse/robbit.yml \
-    --load-vars-from <(lpass show 'Shared-London Services/rabbitmq/robbit-secrets' --notes) \
-    --var github-private-key="$(lpass show 'Shared-London Services/rabbitmq/robbit-github-key' --field="Private Key")"
+  fly -t hh sp -p slacking-robbit -c robbit.yml \
+    --load-vars-from <(lpass show 'Shared-PKS Telemetry/Robbit Pipeline Creds' --notes) \
+    --var github-private-key="$(lpass show 'Shared-PKS Telemetry/[github] pkstelemetrybot-private-key' --notes)"
 fi
